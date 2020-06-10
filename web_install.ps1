0a250f5ba785eff8c15e854eb73af8368451862a
Starlight220 <yotamshlomi@gmail.com>
2020-06-10 18:16:58 +0300

    web install works!

---
+++ web_install.ps1
@@ -0,0 +1,11 @@
+﻿param (
+    [string]$url,
+    [string]$filename,
+    [string]$storageDir = "C:\Users\Yotam\Downloads\AutoInstall"
+)
+Write-Output "$url"
+
+$webclient = New-Object System.Net.WebClient
+$filepath = "$storageDir\$filename"
+Write-Output $filepath
+$webclient.DownloadFile($url,$filepath)
