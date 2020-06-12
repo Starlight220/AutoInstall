3b97af2db1eb95e86c0d4a3b96e7f0cd50b00a40
Starlight220 <yotamshlomi@gmail.com>
2020-06-12 15:54:39 +0300

    rename pt2

---
+++ AutoInstall.psm1
@@ -0,0 +1,13 @@
+﻿function webinstall ([string]$url,
+    [string]$filename,
+    [string]$storageDir = "$Env:Userprofile\Downloads\AutoInstall",
+    [switch]$v) {
+  $filepath = "$storageDir\$filename"
+  if($v) {
+    Write-Output "URL: $url     Filepath: $filepath"
+  } 
+  $webclient = New-Object System.Net.WebClient
+  $webclient.DownloadFile($url,$filepath)
+}
+
+Export-ModuleMember -Function webinstall
